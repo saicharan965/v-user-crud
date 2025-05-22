d3e4246a8b03a3ba014956d343ec25d80328b359
Sai Charan <kutikuppalasaicharan97@gmail.com>
2025-05-23 01:19:31 +0530

    added ci-cd

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'user-management',
 plugins: [vue(), tailwindcss()],
 resolve: {
   alias: {
